2ed6adcf448f4b8e3f5ca8670fb61a50d05c2c88
JonasEmerick <jonasemerick@icloud.com>
2025-11-29 19:28:24 -0300

    Ajuste no funcionamento dos dois get.tasks

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ app.put('/login/:id', async (req, res) => {
 //Consultar tasks por ID
 
 app.get('/getTask/:task_id' , async (req, res) => {
-  const task_id = Number(req.query.task_id);
+  const task_id = Number(req.params.task_id);
   const getTask = await execSQLQueryParams('SELECT * FROM tasks WHERE id = @task_id'
                                          , {task_id}
                                          ) || [];
@@ -196,8 +196,8 @@ app.get('/getTask/:task_id' , async (req, res) => {
   
 
 //Consultar tasks do Usuario
-app.get('/tasks/:user_id', async (req, res) => {
-    const user_id = Number(req.query.user_id);
+app.get('/tasks/:id', async (req, res) => {
+    const user_id = Number(req.params.user_id);
     const aTasks = await execSQLQueryParams(`SELECT * FROM tasks WHERE user_id= @user_id`
                                            , { user_id }
                                            ) || [];
