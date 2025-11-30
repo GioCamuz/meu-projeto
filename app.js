3bdfed6b781124e6502e36580d9cca4586beb9d3
JonasEmerick <jonasemerick@icloud.com>
2025-11-29 22:49:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -298,7 +298,7 @@ app.delete('/tasks/:id', async (req, res) => {
                             , {taskId}
                             );
    
-    if(taskDelete.rowsAffected == [0] ){
+    if(taskDelete.rowsAffected[0] == 0 ){
       return res.status(400).json({ message: 'ID das tarefas não encontrado'});
       
     }
